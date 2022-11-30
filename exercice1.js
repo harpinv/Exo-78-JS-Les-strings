a8eb861e0262a85a81b6b8aee946c4866e19f681
vince <vincent.harpin@orange.fr>
2022-11-30 15:00:38 +0100

    exercice 78

--- exercice1.js
+++ exercice1.js
@@ -1,16 +1,22 @@
+
 let monTexte = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua";
 
 // Completer le code suivant pour afficher la longueur de la variable monTexte
 
-alert(monTexte.);
-
+alert(monTexte.length);
 
 //Ajouter du code ci dessous pour réaliser la suite de l'exercice
 
+let pop = document.getElementById('position');
+pop.innerText = monTexte.indexOf('i');
 
+let myToc = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua";
 
+let chaine = document.getElementById('chaineMaj');
+chaine = myToc.toUpperCase();
 
-
-
-
-
+for(let i = 0; i < monTexte.length; i++) {
+    if(monTexte[i] === "a") {
+        alert("lettre a trouvé");
+    }
+}
